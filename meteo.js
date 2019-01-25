d48a86b4bdf95cfacb09c3485d9c480f7a430417
Giovanni CHARLES-EDOUARD <giovannikce@gmail.com>
2019-01-25 14:20:08 +0100

    Projet

--- meteo.js
+++ meteo.js
@@ -7,44 +7,50 @@ var base_url = "http://api.openweathermap.org/data/2.5/weather";
 var city = "Metz";
 var units = "metric";
 var appid = "621266adac915fdb1f3b829394fd097d";
-var date ;
+var date;
 
 function get_url() {
-    return base_url + "?"
-        + "q=" + city + "&"
-        + "units=" + units + "&"
-        + "appid=" + appid;
+    return base_url + "?" +
+        "q=" + city + "&" +
+        "units=" + units + "&" +
+        "appid=" + appid;
 }
 
-function timeConverter(UNIX_timestamp){
+function timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
     var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
     return time;
-  }
+}
 
 function init_page() {
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("url").innerHTML = get_url();
 
             var response = JSON.parse(this.responseText);
             var temperature = response.main.temp;
+            var vent = response.wind.speed;
+            var humidity = response.main.humidity;
+            var hPa = response.main.pressure;
 
             var icon = response.weather[0].icon;
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
 
             document.getElementById("meteo").innerHTML = temperature;
+            document.getElementById("vent").innerHTML = vent + "km/h";
+            document.getElementById("humidity").innerHTML = humidity + "%";
+            document.getElementById("hPa").innerHTML = hPa + "mbar";
             document.getElementById("icon").src = src;
         }
     };
-    
+
     xhr.open("GET", get_url(), true);
     xhr.send();
 }
@@ -52,14 +58,13 @@ function init_page() {
 function get_temperature() {
     city = document.getElementById("ville").value;
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("url").innerHTML = get_url();
 
-            if(document.getElementById("url_visibility").checked) {
+            if (document.getElementById("url_visibility").checked) {
                 document.getElementById("url").style.display = "block";
-            }
-            else {
+            } else {
                 document.getElementById("url").style.display = "none";
             }
 
@@ -71,25 +76,25 @@ function get_temperature() {
             var date = timeConverter(dt);
             var icon = response.weather[0].icon;
             var main = response.weather[0].main;
-            
-            switch (main){
-                case "Rain" :
+
+            switch (main) {
+                case "Rain":
                     var elem = document.getElementById("main-image");
                     elem.style.backgroundImage = "url('images/rain.jpg')";
                     break;
-                case "Clouds" :
+                case "Clouds":
                     var elem = document.getElementById("main-image");
-                    elem.style.backgroundImage = "url('images/cloud.jpg')";
+                    elem.style.backgroundImage = "url('images/nuages2.jpg')";
                     break;
-                case "Thunderstorm" :
+                case "Thunderstorm":
                     var elem = document.getElementById("main-image");
                     elem.style.backgroundImage = "url('images/thunderstorm.jpg')";
                     break;
-                case "Clear" :
+                case "Clear":
                     var elem = document.getElementById("main-image");
                     elem.style.backgroundImage = "url('images/clear.jpg')";
                     break;
-                case "Atmosphere" :
+                case "Atmosphere":
                     var elem = document.getElementById("main-image");
                     elem.style.backgroundImage = "url('koa.jpg')";
                     break;
@@ -101,7 +106,7 @@ function get_temperature() {
                     var elem = document.getElementById("main-image");
                     elem.style.backgroundImage = "url('images/drizzle.jpg')";
                     break;
-                default :
+                default:
                     var elem = document.getElementById("main-image");
                     elem.style.backgroundImage = "url('images/defaut.jpg')";
                     /*Ensoleillé*/
@@ -109,17 +114,17 @@ function get_temperature() {
 
             }
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
-            
+
             document.getElementById("meteo").innerHTML = temperature;
             document.getElementById("cit").innerHTML = cit;
             document.getElementById("cit").innerHTML += ", " + country;
             document.getElementById("date").innerHTML = date;
             document.getElementById("icon").src = src;
             document.getElementById("ma").innerHTML = main;
-            
+
         }
     };
-    
+
     xhr.open("GET", get_url(), true);
     xhr.send();
 }
